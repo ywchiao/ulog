37a01097f08f7298917b6280409da44f530b3719
Yiwei Chiao <ywchiao@gmail.com>
2018-03-30 10:12:21 +0800

    move .babel setting from package.json -> rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,16 @@
+/**
+ *  @file       rollup.config.js
+ *  @brief      The Configuration file for the rollup.js
+ *  @author     Yiwei Chiao (ywchiao@gmail.com)
+ *  @date       03/12/2018 created.
+ *  @date       03/30/2018 last modified.
+ *  @version    0.1.0
+ *  @since      0.1.0
+ *  @copyright  MIT, © 2018 Yiwei Chiao
+ *  @details
+ *
+ *  The Configuration file for the rollup.js
+ */
 
 import babel from 'rollup-plugin-babel';
 
@@ -10,7 +23,26 @@ export default {
   },
   plugins: [
     babel({
+      babelrc: false,
       exclude: 'node_modules/**',
-    })
+      presets: [
+        [
+          "env",
+          {
+            targets: {
+              browsers: [
+                "last 2 versions"
+              ]
+            },
+            modules: false
+          }
+        ]
+      ],
+      plugins: [
+        "external-helpers"
+      ]
+    }),
   ],
 };
+
+// rollup.config.js
